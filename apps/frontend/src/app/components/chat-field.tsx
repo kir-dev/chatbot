900ef1d0e67672c6327740407ce00b947249aa3b
Attila Sax <saxattila@gmail.com>
2025-08-31 18:32:56 +0200

    added scrolling

--- apps/frontend/src/app/components/chat-field.tsx
+++ apps/frontend/src/app/components/chat-field.tsx
@@ -3,13 +3,15 @@ import MessageCard from './message-card';
 
 export default function ChatField({ chat }: { chat: Chat }) {
   return (
-    <div className='h-full w-full flex flex-col'>
-      <div className='flex-1'></div>
-      {chat.messages
-        .sort((a, b) => a.index - b.index)
-        .map((message) => (
-          <MessageCard key={message.id} message={message} />
-        ))}
+    <div className='h-full w-full flex flex-col px-10 overflow-y-scroll'>
+      <div className='flex-1' />
+      <div className='space-y-5'>
+        {chat.messages
+          .sort((a, b) => a.index - b.index)
+          .map((message) => (
+            <MessageCard key={message.id} message={message} />
+          ))}
+      </div>
     </div>
   );
 }
