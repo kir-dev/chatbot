2fb994b5b982446be64b2137e8e63afbb6384bc9
Attila Sax <saxattila@gmail.com>
2025-09-05 11:26:56 +0200

    corrected type

--- apps/frontend/src/app/components/message-card.tsx
+++ apps/frontend/src/app/components/message-card.tsx
@@ -1,14 +1,12 @@
 import { LuMessageSquare } from 'react-icons/lu';
-import { Message, SentBy } from '../types/message';
+import { Message } from '../types/message';
 
 export default function MessageCard({ message }: { message: Message }) {
   return (
-    <div className={`flex ${message.sentBy == SentBy.USER ? 'flex-row' : 'flex-row-reverse'}`}>
+    <div className={`flex ${!message.sentByBot ? 'flex-row' : 'flex-row-reverse'}`}>
       <div className='flex-1 min-w-1/6' />
-      <div
-        className={`flex flex-row p-2 rounded-lg ${message.sentBy == SentBy.USER ? 'bg-green-400 text-black' : 'text-white'}`}
-      >
-        {message.sentBy == SentBy.BOT && <LuMessageSquare size={48} className='min-w-12 mr-2 text-green-400' />}
+      <div className={`flex flex-row p-2 rounded-lg ${!message.sentByBot ? 'bg-green-400 text-black' : 'text-white'}`}>
+        {message.sentByBot && <LuMessageSquare size={48} className='min-w-12 mr-2 text-green-400' />}
         <p className=''>{message.message}</p>
       </div>
     </div>
--- apps/frontend/src/app/types/message.ts
+++ apps/frontend/src/app/types/message.ts
@@ -1,12 +1,7 @@
-export enum SentBy {
-  USER,
-  BOT,
-}
-
 export type Message = {
   id: number;
   message: string;
   createdAt: Date;
   index: number;
-  sentBy: SentBy;
+  sentByBot: boolean;
 };
