c46d010ea2987b4602e6579f0a523bf153f10fd8
Attila Sax <saxattila@gmail.com>
2025-09-05 11:27:06 +0200

    allow undefined chat

--- apps/frontend/src/app/components/chat-field.tsx
+++ apps/frontend/src/app/components/chat-field.tsx
@@ -1,16 +1,14 @@
 import { Chat } from '../types/chat';
 import MessageCard from './message-card';
 
-export default function ChatField({ chat }: { chat: Chat }) {
+export default function ChatField({ chat }: { chat: Chat | undefined }) {
   return (
     <div className='h-full w-full flex flex-col p-10 overflow-y-scroll'>
       <div className='flex-1 min' />
       <div className='space-y-5'>
-        {chat.messages
+        {chat?.messages
           .sort((a, b) => a.index - b.index)
-          .map((message) => (
-            <MessageCard key={message.id} message={message} />
-          ))}
+          .map((message) => <MessageCard key={message.id} message={message} />)}
       </div>
     </div>
   );
